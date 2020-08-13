69a0ac0f63315e77076049dd40be8f2e73f9f871
Annastasshia Ames <aames26@gmail.com>
2020-08-13 14:44:12 -0400

    npm / directory setups / JS outlining

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,5 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// extend employee
+school
+getSchool()
+getRole() // Overridden to return 'Intern'
